a91c4b774ce2425948e1a59681632b21c7692055
Shafak93 <shafakctg@gmail.com>
2022-10-24 01:18:25 +0600

    producs model added

---
+++ models/products.model.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    name: "Sirajul Islam",
+    age: "74",
+  },
+  {
+    name: "Morsheda Begum",
+    age: "45",
+  },
+  {
+    name: "Nargis Begum",
+    age: "45",
+  },
+  {
+    name: "Mohiuddin Zakaria",
+    age: "40",
+  },
+  {
+    name: "Mohiuddin Shafak",
+    age: "32",
+  },
+];
+module.exports = products;
